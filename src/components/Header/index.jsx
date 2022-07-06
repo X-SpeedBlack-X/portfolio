278b136154dfad8bb50c8f5303e7e807c57b30e2
Isaque.S <isaqueblack129@gmail.com>
2022-07-06 13:04:59 -0300

    feat: add profile and nav in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,16 @@
+import { Nav } from '../Nav'
+import { Profile } from '../Profile'
+
 export function Header() {
   return (
-    <>
-      <div className="">oi</div>
+    <div>
+      <Nav />
+      <Profile />
 
-      <div>
-        <div></div>
+      <div className="flex-1 flex flex-col items-center absolute top-24 right-[30rem] mt-8">
+        <h2 className="text-3xl">Hello, Friend</h2>
+        <p>Seja bem-vindo ao meu portfolio</p>
       </div>
-    </>
+    </div>
   )
 }
