17e8fc6e4bfb12e4cf924f56e89d1f6d5ba13ea7
Isaque.S <isaqueblack129@gmail.com>
2022-07-10 15:07:22 -0300

    feat(profile): news features

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,28 +1,35 @@
-import { LocationMarkerIcon, MailIcon } from '@heroicons/react/outline'
+import { LocationMarkerIcon } from '@heroicons/react/outline'
 
-export function Profile({ icon, textIcon, mail, ...rest }) {
+export function Profile({ icon, textIcon }) {
   return (
-    <div className="w-[22rem] h-80 bg-gray-700 rounded-lg m-8 flex flex-col items-center text-center justify-center">
+    <div className="md:flex rounded-xl p-8 md:p-0 dark:bg-slate-800 mt-6">
       <img
-        className="w-32 mb-4 rounded-full border-green-400 border-2"
+        className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
         src="assets/img.jpeg"
-        alt="foto de perfil"
+        alt=""
+        width="384"
+        height="512"
       />
-      <div {...rest}>
-        <h1>Isaque de Sousa</h1>
-        <span className="opacity-40 text-green-300">Front-end Developer</span>
-        <span className="flex items-center justify-center text-center gap-1 mt-2">
-          {(icon = <LocationMarkerIcon className="w-4 h-4" />)}
-          {(textIcon = 'Brasil')}
-        </span>
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <p className="text-lg text-gray-200 font-medium">
+          Sou um desenvolvedor Front-end em{' '}
+          <span className="text-sky-500">ReactJS</span>. Focado em aprender,
+          resolver problemas e a passar a melhor qualidade possível. Sou um
+          entusiasta da tecnologia, principalmente da programação.
+        </p>
+
+        <figcaption className="font-medium">
+          <div className="text-gray-200">Isaque de Sousa</div>
+          <span className="opacity-40 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Front-end Developer
+          </span>
+
+          <div className="flex items-center justify-center md:justify-start text-gray-200 mt-3 gap-1">
+            {(icon = <LocationMarkerIcon className="w-4 h-4 " />)}
+            {(textIcon = 'Brasil')}
+          </div>
+        </figcaption>
       </div>
     </div>
   )
-}
-
-{
-  /* <span className="flex items-center text-center gap-2 mt-2 ">
-{(icon = <MailIcon className="w-4 h-4" />)}
-{(mail = 'isaque.error404@gmail.com')}
-</span> */
 }
