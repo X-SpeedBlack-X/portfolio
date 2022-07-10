b11f22f3d3b000f623fa39039b203f00460f6a84
Isaque.S <isaqueblack129@gmail.com>
2022-07-10 15:04:56 -0300

    feat: add news animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        linearBg: 'linear-gradient(to right, #1b1429, #140f23) '
+      },
+
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-30deg)' },
+          '50%': { transform: 'rotate(9deg)' }
+        }
+      },
+
+      animation: {
+        wave: 'wiggle 1s ease-in-out infinite'
+      },
+
       colors: {
         green: {
           300: '#00B37E',
@@ -18,6 +33,14 @@ module.exports = {
         red: {
           500: '#F75A68'
         },
+        violet: {
+          800: '#1b1429',
+          900: '#140f23'
+        },
+        purple: {
+          200: '#cd5ff8',
+          800: '#654384'
+        },
         gray: {
           100: '#E1E1E6',
           200: '#C4C4CC',
@@ -25,7 +48,8 @@ module.exports = {
           500: '#323238',
           600: '#29292E',
           700: '#121214',
-          900: '#09090A'
+          900: '#09090A',
+          1000: '#202024'
         }
       }
     }
