1da9aaf0f1133e6afa31477e835947dddc6d629b
Isaque.S <isaqueblack129@gmail.com>
2022-07-10 15:02:01 -0300

    feat(pages): create pages, add news files and  add components

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import { Profile } from '../components/Profile'
+
+export function About() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { HomeContainer } from '../components/Home'
+
+export function Home() {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  )
+}
