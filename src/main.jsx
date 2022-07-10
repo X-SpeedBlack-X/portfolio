3d0e0044b31d267eabf11983b7b8f2578c9fe299
Isaque.S <isaqueblack129@gmail.com>
2022-07-10 15:05:28 -0300

    feat: import scrollbar style

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style/global.css'
+import './style/scrollbar.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
