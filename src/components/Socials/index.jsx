c73b15d1918db949457af33339b0cd45c5f07a01
Isaque.S <isaqueblack129@gmail.com>
2022-07-11 19:44:45 -0300

    feat(socials): add framer motion and add linear gradient background

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -1,37 +1,46 @@
 import { GithubLogo, InstagramLogo, LinkedinLogo } from 'phosphor-react'
+import { motion } from 'framer-motion'
 
 export function Socials() {
   return (
     <>
-      <div className="text-3xl mt-11 ">
-        <h2>Como me encontrar</h2>
-      </div>
+      <motion.div
+        animate="visible"
+        initial="hidden"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1, transition: { duration: 1 } }
+        }}
+        className=" flex flex-col text-center justify-center mt-11  "
+      >
+        <h2 className="text-3xl">Como me encontrar</h2>
 
-      <p>
-        Sinta-se à vontade para se
-        <span className="text-purple-500"> conectar </span>comigo
-      </p>
+        <p>
+          Sinta-se à vontade para se
+          <span className="text-purple-500"> conectar </span>comigo
+        </p>
 
-      <div className="flex gap-5 ">
-        <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-purple-900 rounded-full w-11 h-11">
-          <a href="https://github.com/x-speedblack-x">
-            {' '}
-            <GithubLogo size={32} weight="regular" />
-          </a>
-        </i>
+        <div className="flex justify-center gap-5 mt-4 ">
+          <i className="flex justify-center items-center transition-colors  bg-purple-500 hover:bg-black-200 rounded-full w-11 h-11">
+            <a href="https://github.com/x-speedblack-x">
+              {' '}
+              <GithubLogo size={32} weight="regular" />
+            </a>
+          </i>
 
-        <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-purple-900 rounded-full w-11 h-11">
-          <a href="https://linkedin.com/in/isaque-de-sousa">
-            <LinkedinLogo size={32} weight="regular" />
-          </a>
-        </i>
+          <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-blue-600 rounded-full w-11 h-11">
+            <a href="https://linkedin.com/in/isaque-de-sousa">
+              <LinkedinLogo size={32} weight="regular" />
+            </a>
+          </i>
 
-        <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-purple-900 rounded-full w-11 h-11">
-          <a href="https://instagram.com/_isaque.s_">
-            <InstagramLogo size={32} weight="regular" />
-          </a>
-        </i>
-      </div>
+          <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-instagramBg rounded-full w-11 h-11">
+            <a href="https://instagram.com/_isaque.s_">
+              <InstagramLogo size={32} weight="regular" />
+            </a>
+          </i>
+        </div>
+      </motion.div>
     </>
   )
 }
