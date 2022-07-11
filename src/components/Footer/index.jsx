7537ccc91aec246a8430f845f4c88601abf5bc32
Isaque.S <isaqueblack129@gmail.com>
2022-07-11 19:39:49 -0300

    feat(footer): create footer and add news features

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+export function Footer() {
+  return (
+    <footer className="mt-6 h-16 bg-[rgba(0,0,0,0.3)] flex items-center justify-center py-12  ">
+      <div className="lg:flex gap-7">
+        <p>Desenvolvedor Web e Mobile</p>
+
+        <p>
+          Criado com ♥ pelo{' '}
+          <a href="https://github.com/x-speedblack-x" target="_blank">
+            <span className="text-purple-600 hover:text-purple-200">
+              Isaque.S
+            </span>
+          </a>{' '}
+        </p>
+        <p>Copyright © 2022 </p>
+      </div>
+    </footer>
+  )
+}
