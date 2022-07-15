132c08ba0f1820b1d923302877ddcc04c2f553a9
Isaque.S <isaqueblack129@gmail.com>
2022-07-15 18:31:02 -0300

    feat(routes): add eslint config and add footer

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes as Router, Route } from 'react-router-dom'
-import { Nav } from './components/Nav'
-import { Projects } from './components/Projects'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
+import { BrowserRouter, Routes as Router, Route } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { Nav } from './components/Nav';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { Projects } from './pages/Projects';
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Nav />
+
       <Router>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
       </Router>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
