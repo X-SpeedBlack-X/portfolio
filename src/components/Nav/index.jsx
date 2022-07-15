0979730ac9920d4a3490afbec2ad00b9f0b3d5ac
Isaque.S <isaqueblack129@gmail.com>
2022-07-15 18:24:35 -0300

    feat(nav): refactor nav and add eslint

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,81 +1,114 @@
 import {
-  HomeIcon,
   UserCircleIcon,
   DesktopComputerIcon,
   MailIcon,
   PresentationChartLineIcon,
   MenuAlt3Icon,
-  XIcon
-} from '@heroicons/react/outline'
-import { GitFork, Star } from 'phosphor-react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
+  XIcon,
+  HomeIcon,
+} from '@heroicons/react/outline';
+import { GitFork, Star } from 'phosphor-react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const NAVLINKS = [
+  {
+    id: 1,
+    href: '/',
+    icon: HomeIcon,
+    text: 'Home',
+  },
+  {
+    id: 2,
+    href: '/about',
+    icon: UserCircleIcon,
+    text: 'Sobre mim',
+  },
+
+  {
+    id: 3,
+    href: '/projects',
+    icon: DesktopComputerIcon,
+    text: 'Projetos',
+  },
+
+  {
+    id: 4,
+    href: '/experience',
+    icon: PresentationChartLineIcon,
+    text: 'Experiencia',
+  },
+
+  {
+    id: 5,
+    href: '/contact',
+    icon: MailIcon,
+    text: 'Contato',
+  },
+];
+function NavLink({ href, icon: Icon, text }) {
+  return (
+    <li className="hover:text-purple-200 hover:border-b-4 hover:border-b-purple-200 transition-colors ">
+      <Link to={href} className="flex gap-1">
+        <Icon className="w-6 h-6" /> {text}
+      </Link>
+    </li>
+  );
+}
 
 export function Nav() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+  const [showNav, setShowNav] = useState(false);
   return (
     <motion.header
       animate="visible"
       initial="hidden"
       variants={{
         hidden: { opacity: 0 },
-        visible: { opacity: 1, transition: { duration: 1 } }
+        visible: { opacity: 1, transition: { duration: 1 } },
       }}
       className="border-b border-b-gray-300/30"
     >
-      <nav className=" flex items-center  justify-between m-2 h-16 p-7 text-lg ">
-        <span className=" text-4xl text-purple-200  animate-pulse ">
+      <nav className=" flex items-center justify-between m-2 h-16 p-7 text-lg ">
+        <span className=" text-4xl text-purple-200  animate-pulse">
           <Link to="/">&lt;IS &#47;&gt;</Link>
         </span>
 
         <button
+          type="button"
           className="block w-12 h-12 lg:hidden "
           onClick={() => setToggle(!toggle)}
         >
           {toggle ? (
-            <XIcon className="text-white" />
+            <XIcon
+              className="text-white"
+              onClick={() => setShowNav(!showNav)}
+            />
           ) : (
             <MenuAlt3Icon className="text-white" />
           )}
         </button>
 
         <ul className="hidden lg:flex lg:items-center h-14 gap-9 p-6">
-          <li className="hover:text-purple-200 transition-colors ">
-            <Link to="/" className="flex gap-1">
-              <HomeIcon className="w-6 h-6" /> Home
-            </Link>
-          </li>
-          <li className="hover:text-purple-200 transition-colors">
-            <Link to="/about" className="flex gap-1">
-              <UserCircleIcon className="w-6 h-6" /> Sobre mim
-            </Link>
-          </li>
-          <li className="hover:text-purple-200 transition-colors">
-            <Link to="/projects" className="flex gap-1">
-              <DesktopComputerIcon className="w-6 h-6" /> Projetos
-            </Link>
-          </li>
-          <li className="hover:text-purple-200 transition-colors">
-            <Link to="/experience" className="flex gap-1">
-              {' '}
-              <PresentationChartLineIcon className="w-6 h-6" /> Experiência
-            </Link>
-          </li>
-          <li className="hover:text-purple-200 transition-colors">
-            <Link to="/contact" className="flex gap-1">
-              <MailIcon className="w-6 h-6" /> Contato
-            </Link>
-          </li>
+          {NAVLINKS.map((link) => (
+            <NavLink
+              href={link.href}
+              icon={link.icon}
+              text={link.text}
+              key={link.id}
+            />
+          ))}
 
           <i
-            className="flex justify-center bg-purple-800 hover:bg-purple-200 
+            className="flex justify-center bg-purple-800 hover:bg-purple-200
           hover:top-[-3px] relative transition-colors w-20 h-9  rounded-md"
           >
             <a
               href="https://github.com/x-speedblack-x/portfolio"
               className="flex items-center justify-center gap-1"
               target="_blank"
+              rel="noreferrer"
             >
               {' '}
               <GitFork size={25} weight="duotone" />
@@ -85,5 +118,5 @@ export function Nav() {
         </ul>
       </nav>
     </motion.header>
-  )
+  );
 }
