b3d406b7a384d98ce225a2225eeb027c93a7b1f2
Isaque.S <isaqueblack129@gmail.com>
2022-07-17 13:15:11 -0300

    feat(social): install clsx, refactor and add variants

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -1,5 +1,52 @@
 import { GithubLogo, InstagramLogo, LinkedinLogo } from 'phosphor-react';
 import { motion } from 'framer-motion';
+import clsx from 'clsx';
+
+const SOCIALLINKS = [
+  {
+    id: 1,
+    icon: GithubLogo,
+    href: 'https://github.com/x-speedblack-x',
+    target: '_blank',
+    variant: 'black',
+  },
+
+  {
+    id: 2,
+    icon: LinkedinLogo,
+    href: 'https://linkedin.com/in/isaque-de-sousa',
+    target: '_blank',
+  },
+
+  {
+    id: 2,
+    icon: InstagramLogo,
+    href: 'https://instagram.com/_isaque.s_',
+    target: '_blank',
+    variant: 'secondary',
+  },
+];
+
+const Variants = {
+  primary: 'hover:bg-blue-600',
+  secondary: 'hover:bg-instagramBg ',
+  black: 'hover:bg-black-200',
+};
+
+function SocialLinks({ icon: Icon, href, target, variant = 'primary' }) {
+  return (
+    <i
+      className={clsx(
+        'flex justify-center items-center transition-colors  bg-purple-500 rounded-full w-11 h-11',
+        Variants[variant]
+      )}
+    >
+      <a href={href} target={target}>
+        <Icon size={32} weight="regular" />
+      </a>
+    </i>
+  );
+}
 
 export function Socials() {
   return (
@@ -19,25 +66,16 @@ export function Socials() {
         <span className="text-purple-500"> conectar </span>comigo
       </p>
 
-      <div className="flex justify-center gap-5 mt-4 ">
-        <i className="flex justify-center items-center transition-colors  bg-purple-500 hover:bg-black-200 rounded-full w-11 h-11">
-          <a href="https://github.com/x-speedblack-x">
-            {' '}
-            <GithubLogo size={32} weight="regular" />
-          </a>
-        </i>
-
-        <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-blue-600 rounded-full w-11 h-11">
-          <a href="https://linkedin.com/in/isaque-de-sousa">
-            <LinkedinLogo size={32} weight="regular" />
-          </a>
-        </i>
-
-        <i className="flex justify-center items-center transition-colors bg-purple-500 hover:bg-instagramBg rounded-full w-11 h-11">
-          <a href="https://instagram.com/_isaque.s_">
-            <InstagramLogo size={32} weight="regular" />
-          </a>
-        </i>
+      <div className="flex justify-center gap-5 mt-4 hover:backgro ">
+        {SOCIALLINKS.map((social) => (
+          <SocialLinks
+            key={social.id}
+            icon={social.icon}
+            href={social.href}
+            target={social.target}
+            variant={social.variant}
+          />
+        ))}
       </div>
     </motion.div>
   );
