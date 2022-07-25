7a280182375ea00578565781b8e17cd8ea03a76f
Isaque.S <isaqueblack129@gmail.com>
2022-07-25 11:20:44 -0300

    feat(motion): add eslint

--- src/components/Motion/index.jsx
+++ src/components/Motion/index.jsx
@@ -1,18 +1,18 @@
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 export function Motion({ children, className }) {
   return (
     <motion.div
       animate="translateEnd"
       initial="translateInitial"
-      transition={{ duration: 0.7 }}
+      transition={{ duration: 0.6 }}
       variants={{
         translateEnd: { translateY: 0 },
-        translateInitial: { translateY: 290 }
+        translateInitial: { translateY: 290 },
       }}
       className={className}
     >
       {children}
     </motion.div>
-  )
+  );
 }
