0df2d0eb39ee3f0481a5631e23a6c167ff8eb016
Isaque.S <isaqueblack129@gmail.com>
2022-07-25 11:24:56 -0300

    feat(routes): add new routes scroll

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes as Router, Route } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Nav } from './components/Nav';
+import { Scroll } from './components/Scroll';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
@@ -10,13 +11,14 @@ export function Routes() {
   return (
     <BrowserRouter>
       <Nav />
-
-      <Router>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Router>
+      <Scroll>
+        <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Router>
+      </Scroll>
       <Footer />
     </BrowserRouter>
   );
