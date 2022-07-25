6f03f598cc6cab2b84c342d768951d44c4545b30
Isaque.S <isaqueblack129@gmail.com>
2022-07-25 11:22:59 -0300

    feat(skillcard): refactoring

--- src/components/SkillsCards/index.jsx
+++ src/components/SkillsCards/index.jsx
@@ -13,48 +13,72 @@ import {
   SiTypescript,
 } from 'react-icons/si';
 
+const SKILL_CARD = [
+  {
+    id: 1,
+    icon: AiFillHtml5,
+  },
+
+  {
+    id: 2,
+    icon: DiCss3Full,
+  },
+
+  {
+    id: 3,
+    icon: DiJavascript1,
+  },
+
+  {
+    id: 4,
+    icon: SiTypescript,
+  },
+
+  {
+    id: 5,
+    icon: DiReact,
+  },
+
+  {
+    id: 6,
+    icon: DiNodejs,
+  },
+
+  {
+    id: 7,
+    icon: DiGit,
+  },
+
+  {
+    id: 8,
+    icon: SiTailwindcss,
+  },
+
+  {
+    id: 9,
+    icon: SiStyledcomponents,
+  },
+
+  {
+    id: 10,
+    icon: SiChakraui,
+  },
+];
+
+function SkillCardProject({ icon: Icon }) {
+  return (
+    <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
+      <Icon size={90} />
+    </div>
+  );
+}
+
 export function SkillCard() {
   return (
-    <div className="grid lg:grid-cols-4 grid-cols-2 w-2/3">
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <AiFillHtml5 size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <DiCss3Full size={90} />
-      </div>
-
-      <div className="m-4 p-5 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <DiJavascript1 size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <SiTypescript size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <DiReact size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <DiNodejs size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <DiGit size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <SiTailwindcss size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <SiStyledcomponents size={90} />
-      </div>
-
-      <div className="m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all">
-        <SiChakraui size={90} />
-      </div>
+    <div className="grid lg:grid-cols-5 grid-cols-2 w-2/3">
+      {SKILL_CARD.map((skill) => (
+        <SkillCardProject icon={skill.icon} key={skill.id} />
+      ))}
     </div>
   );
 }
