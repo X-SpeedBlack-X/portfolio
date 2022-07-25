404fb3d6be06d9171c38d606074a594730eaa777
Isaque.S <isaqueblack129@gmail.com>
2022-07-25 11:19:56 -0300

    feat(projects): add useEffect, fetch and refactoring

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,36 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Motion } from '../Motion';
 
 export function ProjectsContainer() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/X-SpeedBlack-X/repos').then(
+      async (response) => {
+        const projectGitHub = await response.json();
+        setProjects(projectGitHub);
+      }
+    );
+  }, []);
+
   return (
     <Motion className="mt-10 p-[45px]">
-      <div className="flex flex-col justify-center items-center text-center">
+      <div className="flex flex-col justify-center items-center text-center mt-16">
         <h2 className="text-4xl">
           Meus <span className="text-purple-200">projetos</span>
         </h2>
         <p className="text-lg p-4">
-          Aqui estão alguns projetos em que trabalhei recentemente
+          Aqui estão alguns projetos em que trabalhei
         </p>
       </div>
       <div className="flex justify-center">
-        {' '}
         <img
           className="animate-float "
           src="assets/computerFloat.png"
-          alt=""
+          alt="Foto de um notebook"
           width={200}
           height={200}
         />
       </div>
-      {/*  <div className="grid lg:grid-cols-3 grid-cols-1 gap-8 ">
-          <div className="bg-slate-900 p-3 flex flex-col items-center justify-center rounded-lg">
-            <img src="assets/ignitelab.jpeg" alt="" width={200} />
-            <h2>Uma plataforma de evento</h2>
+      <div className="grid grid-cols-3 grid-rows-3">
+        <div className="bg-green-500">
+          {projects.map((project) => (
+            <p key={project.id}>{project.name}</p>
+          ))}
+        </div>
+
+        <div className='m-4 p-3 border-[1px] border-[#c889e6a2] rounded-md shadow-lg shadow-purple-500/30 lg:opacity-40 hover:opacity-80 hover:scale-110 hover:border-purple-200 hover:border-2 flex justify-center items-center transition-all"'>
+          <div>
+            {' '}
+            {projects.map((project) => (
+              <p key={project.id[2]}>{project.name}</p>
+            ))}
           </div>
-          <div className="bg-slate-900">io</div>
-          <div className="bg-slate-900">io</div>
-          <div className="bg-slate-900">io</div>
-          <div className="bg-slate-900">io</div>
-        </div> */}
+        </div>
+      </div>
     </Motion>
   );
 }
